486bf6c3c487f779e67a0f8cea7896c052ef01c8
jalal-codr <abduljalal849@gmail.com>
2026-04-01 09:10:57 +0100

    Initial Commit

---
+++ code buddy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  code buddy
+//
+//  Created by MACS on 01/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ code buddy/code_buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  code_buddyApp.swift
+//  code buddy
+//
+//  Created by MACS on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct code_buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
